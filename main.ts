d010ec6c422e866b030982427398cf3e3e7bf4ab
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-10-31 18:15:39 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let x_value = 0
+let y_value = 0
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     x_value = 0
@@ -21,9 +23,9 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(500)
         x_value += 1
     }
-    x_value = 4
-    y_value += 1
     for (let index = 0; index < 2; index++) {
+        x_value = 4
+        y_value += 1
         for (let index = 0; index < 5; index++) {
             led.plot(x_value, y_value)
             basic.pause(500)
@@ -33,18 +35,8 @@ input.onButtonPressed(Button.B, function () {
         x_value += 1
         for (let index = 0; index < 5; index++) {
             led.plot(x_value, y_value)
-            y_value += -1
             basic.pause(500)
-            x_value += -1
+            x_value += 1
         }
     }
-    for (let index = 0; index < 5; index++) {
-        led.plot(x_value, y_value)
-        basic.pause(500)
-        x_value += -1
-    }
 })
-let y_value = 0
-let x_value = 0
-x_value = 5
-y_value = 0
