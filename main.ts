c7fbae808c149b0ff1e6d615c8cfa3f43ecd93a6
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-10-31 17:43:12 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    x_value = 0
+    y_value = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x_value, y_value)
+            basic.pause(500)
+            x_value += 1
+        }
+        y_value += 1
+        x_value = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    x_value = 0
+    y_value = 0
+    for (let index = 0; index < 5; index++) {
+        led.plot(x_value, y_value)
+        basic.pause(500)
+        x_value += 1
+    }
+    x_value = 4
+    y_value += 1
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x_value, y_value)
+            basic.pause(500)
+            x_value += -1
+        }
+        y_value += 1
+        x_value += 1
+        for (let index = 0; index < 5; index++) {
+            led.plot(x_value, y_value)
+            y_value += -1
+            basic.pause(500)
+            x_value += -1
+        }
+    }
+    for (let index = 0; index < 5; index++) {
+        led.plot(x_value, y_value)
+        basic.pause(500)
+        x_value += -1
+    }
+})
+let y_value = 0
+let x_value = 0
+x_value = 5
+y_value = 0
